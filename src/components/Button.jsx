3befcefd34826fe6867bea510c0b0744b567dcfc
lee123so <100946922+lee123so@users.noreply.github.com>
2022-09-24 20:29:13 +0900

    feat : 버튼 컴포넌트 추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { colors } from '../lib/constants/GlobalStyle'
+
+const SIZES = {
+    FixedBtn: css`
+        
+    `,
+    Caption: css`
+        
+    `,
+    Content: css`
+        
+    `
+}
+
+
+const Button = () => {
+    return (
+        <StyledButton disabled={disabled}>{children}</StyledButton>
+    )
+}
+
+export default Button
+const StyledButton = styled.button`
+    cursor: pointer;
+    font-size: var(--button-font-size, 1rem);
+    padding: var(--button-padding, 12px 16px);
+    background: var(--button-bg-color, ${colors.secondary});
+    color: var(--button-color, #ffffff);
+
+    &:active,
+    &:hover,
+    &:focus {
+        background: var(--button-hover-bg-color, ${colors.primary});
+    }
+
+    &:disabled {
+        cursor: default;
+        opacity: 0.5;
+        background: var(--button-bg-color, #025ce2);
+    }
+    `;
