790a77a208ac93dab70aacae57e7fa7019648eff
lee123so <100946922+lee123so@users.noreply.github.com>
2022-10-26 12:38:35 +0900

    feat : 내가 찜한 게시물 내가 작성한 게시물 수정 삭제 기능 추가

--- src/pages/MyLikePost/components/MyLikeCard.jsx
+++ src/pages/MyLikePost/components/MyLikeCard.jsx
@@ -5,25 +5,42 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { _deletePost } from '../../../redux/modules/formSlice';
 import { _getLikePost } from '../../../redux/modules/homeSilce';
+import Loading from '../../Loading/Loading';
+import HomeCard from '../../Home/components/HomeCard';
+import styled from '@emotion/styled';
+import { colors, fonts } from '../../../lib/constants/GlobalStyle';
 const MyLikeCard = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [coseId,setCoseId] = useState();
-    const {detail,isLoding,error} = useSelector((state)=>state.homeSlice.detail)
-    // console.log(post)
-if(isLoding){
-    <div>로딩중...</div>
-}
+    const {detail,isLoding,error} = useSelector((state)=>state.homeSlice)
+    console.log(useSelector((state)=>state.homeSlice))
+
 useEffect(()=>{
     dispatch(_getLikePost())
 },[])
 
     return (
         <div >
-            <h1>내가 작성한 게시물</h1>
-            {/* {detail.map((post)=>)} */}
+            {isLoding ?<Loading/> : null}
+            <StLikePostContainer>
+            <h1>내가 찜 한 게시물</h1>
+            </StLikePostContainer>
+            {detail?.map((post)=>
+            <HomeCard id ={post.courseId} post= {post} />
+            )}
         </div>
     )
 }
 
 export default MyLikeCard
+const StLikePostContainer = styled.div`
+margin-top: 50px;
+padding: 30px;
+  width: 100%;
+  text-align: center;
+  h1{
+    font-size: ${fonts.subTitle};
+    color: ${colors.deepGray};
+  }
+`
--- src/pages/MypagePost/components/MyPost.jsx
+++ src/pages/MypagePost/components/MyPost.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import axios from 'axios';
 import { useEffect,useState } from 'react';
 import MyPostCard from './MyPostCard';
+import styled from '@emotion/styled';
+import { fonts,colors } from '../../../lib/constants/GlobalStyle';
+import HomeCard from '../../Home/components/HomeCard';
 const MyPost = () => {
     const accessToken = localStorage.getItem('Authorization'); //accessToken
     const refreshToken = localStorage.getItem('RefreshToken'); //refreshToken
@@ -18,13 +21,10 @@ const MyPost = () => {
           console.log(res.data)
           if (res.status = 200 || 201){
             setMyPost(res.data);  
-      
           }
         }catch(err){
           console.error(err.response);
         }
-    
-        
       };
       console.log("work!",myPost);
     useEffect(()=>{
@@ -32,12 +32,24 @@ const MyPost = () => {
     },[])
   return (
     <div>
+      <StLikePostContainer>
         <h1>내가 작성한 게시물</h1>
-        {myPost === undefined ? <div> 작성한 게시물이 없습니다. </div> : myPost.map((post)=>(
-            <MyPostCard key={post.id} post={post}/>
+        </StLikePostContainer>
+        {myPost === undefined ? <div> 작성한 게시물이 없습니다. </div> : myPost.map((post,i)=>(
+            <HomeCard key={post.id} post={post} find ={myPost[i]}/>
         ))}
     </div>
   )
 }
 
 export default MyPost
+const StLikePostContainer = styled.div`
+margin-top: 50px;
+padding: 30px;
+  width: 100%;
+  text-align: center;
+  h1{
+    font-size: ${fonts.subTitle};
+    color: ${colors.deepGray};
+  }
+  `
